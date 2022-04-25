0df961e46b363feb9a3ae064ce1481454fa7ad0f
Hibaq Obsiye <hibaqobsiye@Hibaqs-MacBook-Air.local>
2022-04-25 07:02:49 -0700

    created oystercard file and spec, added balance method

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,3 +1,14 @@
+require 'Oystercard'
+
 describe Oystercard do
+
+    it "checks oystercard has a balance" do
+        oystercard = Oystercard.new
+        expect(subject.balance).to eq(0)
+        
+    end
+    
+    
+    #  expect(subject).to respond_to(:balance).with(1).argument   
 
 end
